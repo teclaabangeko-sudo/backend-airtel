eb2dccc858b84a4ff2ad6fb8feacb3e5b71802db
TeclaAbangeko <teclaabangeko@gmail.com>
2026-03-23 01:19:59 +0100

    fix server crash

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
-
-
-const express = require("express");
+const { createClient } = require('@supabase/supabase-js')
+const jwt = require("jsonwebtoken")
+const express = require("express")
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -10,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// 👉 AJOUT ICI
+
+const SECRET = "rljmkpbdvxvgpwbbmpsm" // 🔴
+
 const supabase = createClient(
   "https://rljmkpbdvxvgpwbbmpsm.supabase.co",   // 🔴 Project URL
   "rljmkpbdvxvgpwbbmpsm" // 🔴  anon key
@@ -21,15 +22,47 @@ const supabase = createClient(
 let promos = [];
 
 // ====== SECURITY ======
-const ADMIN_KEY = "123456"; // change ça plus tard
+const ADMIN_KEY = "Mdp2026@"; // change ça plus tard
 
 // ====== GENERATE CODE ======
 function generateCode() {
   return "PROMO-" + Math.random().toString(36).substring(2, 8).toUpperCase();
 }
 
+app.post("/admin/login", (req, res) => {
+  const { email, password } = req.body
+
+  // 🔴 change ces identifiants
+  if (email === "mdp369@yahoo.fr" && password === "Mdp2026@") {
+    const token = jwt.sign({ role: "admin" }, SECRET, { expiresIn: "2h" })
+
+    return res.json({ token })
+  }
+
+  res.status(401).json({ error: "Invalid credentials" })
+})
+
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token" })
+  }
+
+  const token = authHeader.split(" ")[1]
+
+  try {
+    const decoded = jwt.verify(token, SECRET)
+    req.user = decoded
+    next()
+  } catch (err) {
+    return res.status(403).json({ error: "Invalid token" })
+  }
+}
+
 // ====== CREATE PROMO ======
-app.post("/admin/create-promo", async (req, res) => {
+app.post("/admin/create-promo",  verifyToken, async (req, res) => {
   const { discount, expiresAt, usageLimit } = req.body
 
   const code = "PROMO-" + Math.random().toString(36).substring(2, 8).toUpperCase()
@@ -49,7 +82,7 @@ app.post("/admin/create-promo", async (req, res) => {
 })
 
 // ====== GET PROMOS ======
-app.get("/admin/promos", async (req, res) => {
+app.get("/admin/promos",  verifyToken, async (req, res) => {
   const { data } = await supabase.from("promos").select("*")
   res.json(data)
 })
@@ -121,32 +154,7 @@ app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
 
-app.post("/apply-promo", (req, res) => {
-  const { code, amount } = req.body
 
-  const promo = promos.find(p => p.code === code)
 
-  if (!promo) {
-    return res.json({ valid: false })
-  }
-
-  const now = new Date()
-  const expiry = new Date(promo.expiresAt)
-
-  if (expiry < now) {
-    return res.json({ valid: false })
-  }
-
-  if (promo.used >= promo.usageLimit) {
-    return res.json({ valid: false })
-  }
-
-  const discountAmount = amount * promo.discount
-
-  res.json({
-    valid: true,
-    discountAmount,
-  })
-})
 
 
